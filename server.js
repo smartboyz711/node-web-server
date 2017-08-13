c1a0a00ec4f4815cddef2b01cf462c1701ff1792
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-13 21:35:04 +0700

    server hbs

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = now+':'+req.method+' '+req.url;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err) => {
+      if(err) {
+          console.log('Unable to append to server.log.');
+      }
+  })
+  next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname +'/public'));
 
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+    //return 'test';
+});
+
+hbs.registerHelper('screamIt',(text)=> {
+    return text.toUpperCase();
+})
 app.get('/',(req,res)=>{
     //res.send('<h1>hello Express!</h1>');
-    res.send({
-        name : 'tonz',
-        like : [
-            'Biking',
-            'Cities'
-        ]
-    })
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        massage : 'Welcome to homepage'
+    });
 });
 
 app.get('/about',(req,res) => {
-    res.send('about page');
+    res.render('about.hbs',{
+        pageTitle : 'About Page'
+    });
 });
 
 app.get('/bad',(req,res) => {
